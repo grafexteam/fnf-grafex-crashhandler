261b7d7f74acdc477fa4db2010f1bc246375d0ff
Oleg <MrOlegTitov@yandex.ru>
2022-08-21 23:14:54 +0300

    Fixed command line

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 title Building Grafex Crash Handler...
-pyinstaller --noconsole --name="GrafexCrashHandler" --icon="./resources/icon.ico" --onefile main.py
+pyinstaller --noconsole --name="GrafexCrashHandler" --icon="./resources/icon.ico" --onefile --noupx main.py
 echo Done
 @echo on
